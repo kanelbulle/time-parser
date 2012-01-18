d3213f8804606eca9e47d06443449d3e930e997f
Emil Arfvidsson <emil@arfvidsson.com>
2012-01-18 22:53:21 +0100

    Deleted test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from icalendar import Calendar, Event
-
-icalstring = open('test.ics', 'r').read()
-
-cal = Calendar.from_string(icalstring)	
-for component in cal.walk():
-	print component.decoded('summary', 'none')
